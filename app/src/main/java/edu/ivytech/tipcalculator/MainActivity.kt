4f388f8e4727f6f5e8c1bceee87e0a0638d5c18b
crimshaw <bpeirce@ivytech.edu>
2022-02-09 21:46:43 -0500

    Added radio button handler

--- app/src/main/java/edu/ivytech/tipcalculator/MainActivity.kt
+++ app/src/main/java/edu/ivytech/tipcalculator/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.RadioGroup
 import edu.ivytech.tipcalculator.databinding.ActivityMainBinding
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
+import kotlin.math.ceil
+
+enum class Rounding{NOROUND, ROUNDTIP, ROUNDTOTAL}
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private var tipPercent = 0.15
+    private var rounding : Rounding = Rounding.NOROUND
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -29,6 +34,14 @@ class MainActivity : AppCompatActivity() {
             val percentFormat = NumberFormat.getPercentInstance()
             binding.tipPercentDisplay.text = percentFormat.format(tipPercent)
         }
+
+        binding.radioGroup.setOnCheckedChangeListener { group : RadioGroup, checkedID : Int ->
+            when(checkedID){
+                R.id.noRoundBtn -> rounding = Rounding.NOROUND
+                R.id.roundTipBtn -> rounding = Rounding.ROUNDTIP
+                R.id.roundTotalBtn -> rounding = Rounding.ROUNDTOTAL
+            }
+        }
     }
 
     private fun calculateAndDisplay(){
@@ -40,6 +53,15 @@ class MainActivity : AppCompatActivity() {
 
         var tipAmount = billAmount * tipPercent
         var totalAmount = billAmount + tipAmount
+
+        if (rounding == Rounding.ROUNDTIP){
+            tipAmount = ceil(tipAmount)
+            totalAmount = billAmount + tipAmount
+        }
+        else if (rounding == Rounding.ROUNDTOTAL){
+            totalAmount = ceil(totalAmount)
+            tipAmount = totalAmount - billAmount
+        }
 
         val currencyFormat = NumberFormat.getCurrencyInstance()
         val symbol = DecimalFormatSymbols()
