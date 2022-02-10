5d3bccb2eb4d31cc6f3ac4dfa89fcd097e690370
crimshaw <bpeirce@ivytech.edu>
2022-02-09 21:13:49 -0500

    Added calculate and display function + handler

--- app/src/main/java/edu/ivytech/tipcalculator/MainActivity.kt
+++ app/src/main/java/edu/ivytech/tipcalculator/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import edu.ivytech.tipcalculator.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private var tipPercent = 0.15
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,5 +21,26 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, R.layout.list_item, items)
         binding.splitBillDropdown.setAdapter(adapter)
         binding.splitBillDropdown.setText(items[0], false)
+
+        binding.calculateBtn.setOnClickListener { calculateAndDisplay() }
+    }
+
+    private fun calculateAndDisplay(){
+        val billAmtStr = binding.billAmountEditText.text.toString()
+        var billAmount = 0.0
+        if (billAmtStr.isNotEmpty()){
+            billAmount = billAmtStr!!.toDouble()
+        }
+
+        var tipAmount = billAmount * tipPercent
+        var totalAmount = billAmount + tipAmount
+
+        val currencyFormat = NumberFormat.getCurrencyInstance()
+        val symbol = DecimalFormatSymbols()
+        symbol.currencySymbol = ""
+        (currencyFormat as DecimalFormat).decimalFormatSymbols = symbol
+
+        binding.tipAmountEditText.setText(currencyFormat.format(tipAmount))
+        binding.totalAmountEditText.setText(currencyFormat.format(totalAmount))
     }
 }
